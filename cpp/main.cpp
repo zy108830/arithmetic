532bfdb28dd477bf49bd71358ff06d6d615af26b
siguoya <924714558@qq.com>
2018-02-03 18:34:32 +0800

    cpp选择排序；

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,9 +6,10 @@ void selectorSort(int arr[], int arr_length);
 int main() {
     int arr[] = {7, 1, 9, 5, 3, 4, 8, 2, 6};
     int arr_length=9;
+    std::cout<<"排序后"<<" ";
     selectorSort(arr, arr_length);
-    for (int i = 0; i < arr_length; ++i) {
-        std::cout << i << std::endl;
+    for (int i = 0; i < arr_length; i++) {
+        std::cout << arr[i] << " ";
     }
     return 0;
 }
@@ -18,7 +19,8 @@ void selectorSort(int arr[], int arr_length) {
         int min_value = arr[i];
         for (int j = i + 1; j < arr_length; j++) {
             if (arr[j] < min_value) {
-                swap(min_value, arr[j]);
+                min_value=arr[j];
+                swap(arr[i], arr[j]);
             }
         }
     }
